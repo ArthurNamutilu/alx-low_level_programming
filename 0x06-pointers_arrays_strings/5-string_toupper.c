917e9f932b134cd01b5b2616670580fa467a7d1d
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-03-30 06:08:55 -0700

    changing lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,14 +7,12 @@
 
 char *string_toupper(char *n)
 {
-	int i = 0;
+	int i;
 
-	while (n[i] != '\n')
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		if (n[i] >= 97 && n[i] <= 122)
-/*		if (n[i] >= 'a' && n[i] <= 'z') */
 			n[i] = n[i] - 32;
-		i++;
 	}
 	return (n);
 }
