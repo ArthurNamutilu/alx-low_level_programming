1f9ea9eb25f8399a9da621768990314c022b0426
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-03-30 08:34:12 -0700

    leet code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
-  * leet - replaces
-  * @s: string
-  * Return: replaced some character string.
-  */
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+
 char *leet(char *n)
+
 {
 	int i, j;
-
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-	for (j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == a[j])
 			{
